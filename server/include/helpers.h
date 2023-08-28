8d4d232115147d2fe57aad451c64ccc508df0f14
Ricardo Gatgens <gatgens48@gmail.com>
2023-08-28 12:10:52 -0600

    Calculate the dominant color of an image

---
+++ server/include/helpers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+/**
+* @brief Finds the index of the maximum element in an array.
+* @param array Array of doubles to be searched.
+* @param size Size of the array.
+* @return Index of the maximum element in the array.
+*/
+uint8_t find_max_index(double* array, uint8_t size);
--- server/src/processing.c
+++ server/src/processing.c
@@ -1,12 +1,35 @@
-// #include "processing.h" 
+#include <stdio.h>
+#include <stdlib.h>
 
-// void read_image(string path) {
-//   Mat image = imread(path, IMREAD_COLOR);
-//   if (image.empty()) {
-//     printf("Could not open or find the image");
-//     return;
-//   }
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-//   imshow("Display window", image);
-//   waitKey(0);
-// }
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include "processing.h"
+
+void get_color_sum(char* path, double* color_sum) {
+  int width, height, channels;
+  unsigned char* img = stbi_load(path, &width, &height, &channels, 0);
+
+  if (img == NULL) {
+    printf("Error in loading the image\n");
+    exit(1);
+  }
+
+
+  if (channels > 3) {
+    printf("Image has more than 3 channels\n");
+    exit(1);
+  }
+
+  size_t img_size = width * height * channels;
+  for (size_t i = 0; i < (img_size - 2); i++) {
+    color_sum[0] += ((double)img[i]) / 255;
+    color_sum[1] += ((double)img[i + 1]) / 255;
+    color_sum[2] += ((double)img[i + 2]) / 255;
+  }
+
+  stbi_image_free(img);
+}
